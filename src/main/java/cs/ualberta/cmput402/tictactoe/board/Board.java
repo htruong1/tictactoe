0300d1fc8a9c82b4a69ce5aa8f5876486ea02d01
Cosh_ <cosh@slouple.com>
2019-01-17 15:02:46 -0700

    Added Comment for checkTie. Finalized #1

--- src/main/java/cs/ualberta/cmput402/tictactoe/board/Board.java
+++ src/main/java/cs/ualberta/cmput402/tictactoe/board/Board.java
@@ -57,6 +57,10 @@ public class Board {
 
     }
 
+    /*
+        Check whether there is empty spaces left on the board.
+        If not then it is a tie.
+     */
     private void checkTie(){
         for(int x = 0; x < 3; x++){
             for(int y = 0; y < 3; y++){
